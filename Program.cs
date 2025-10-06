9115910ef50f120cee7788a326b0c954b66a6df7
BoraEfe <Bora.e@icloud.com>
2025-10-06 15:02:48 +0200

    Added .gitignore and cleaned up temporary files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler chocolateBoiler = ChocolateBoiler.GetInstance();
+            chocolateBoiler.fill();
+            chocolateBoiler.boil();
+            chocolateBoiler.drain();
+            chocolateBoiler.printState();
+
+        }
+    }
+}   
